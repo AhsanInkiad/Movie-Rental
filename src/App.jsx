f1803151a7391f6eb683c96755dbf5889f726ee9
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-24 20:59:20 +0600

    Create SideBar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './Header'
+import SideBar from './SideBar'
 
 function App() {
 
@@ -7,6 +8,12 @@ function App() {
     <>
       <>
         <Header></Header>
+        <main>
+          <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <SideBar></SideBar>
+          </div>
+        </main>
+
       </>
     </>
   )
