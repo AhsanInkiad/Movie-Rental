203117cc010bfbf7dad7c9c1857403ba1dca22b6
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-24 20:52:57 +0600

    Create Header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
+import Header from './Header'
 
 function App() {
-  
 
   return (
     <>
-      <div>
-        <p>sdasds</p>
-      </div>
+      <>
+        <Header></Header>
+      </>
     </>
   )
 }
