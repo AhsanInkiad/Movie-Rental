a2b910daf46695c453cbb67326191fcdcaf25667
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-30 13:30:37 +0600

    MovieCard bug fix. (Auto reload issue)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 import MovieList from './cine/MovieList'
+import { MovieContext } from './context'
 import Footer from './footer'
 import Header from './Header'
 import SideBar from './SideBar'
 
 function App() {
-
+  const [cartData, setCartData] = useState([]);
   return (
     <>
       <>
-        <Header></Header>
-        <main>
-          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-            <SideBar></SideBar>
-            <MovieList></MovieList>
-          </div>
-        </main>
-        <Footer></Footer>
+        <MovieContext.Provider value={{cartData, setCartData}}>
+          <Header></Header>
+          <main>
+            <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+              <SideBar></SideBar>
+              <MovieList></MovieList>
+            </div>
+          </main>
+          <Footer></Footer>
+        </MovieContext.Provider>
       </>
     </>
   )
--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -15,12 +15,16 @@ function MovieCard({ movie }) {
         setSelectedMovie(movie);
         setShowModal(true);
     }
+
+    function handleAddToCart(movie){
+
+    }
     return (
         <>
             {showModal && <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose}></MovieDetailsModal>}
             <figure
                 className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-                <a href="" onClick={()=>{handleMovieSelection(movie)}}>
+                <a onClick={()=>{handleMovieSelection(movie)}}>
                     <img className="w-full object-cover" src={getImageUrl(movie.cover)} alt="" />
                     <figcaption className="pt-4">
                         <h3 className="text-xl mb-1">{movie.title}</h3>
@@ -28,11 +32,13 @@ function MovieCard({ movie }) {
                         <div className="flex items-center space-x-1 mb-5">
                             <Rating value={movie.rating}></Rating>
                         </div>
-                        <a className="bg-green-400 rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                            href="#">
+                        <button 
+                        onClick={()=>handleAddToCart(movie)}
+                        className="bg-green-400 rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+                           >
                             <img src="./assets/tag.svg" alt="" />
                             <span>${movie.price} | Add to Cart</span>
-                        </a>
+                        </button>
                     </figcaption>
                 </a>
             </figure>
