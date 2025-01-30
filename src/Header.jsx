3f4790c6f4536e5aa8909b3d574d8f8557b81a17
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-30 13:12:34 +0600

    Show cart dynamically.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './assets/logo.svg'
 import Ring from './assets/ring.svg'
 import Moon from './assets/icons/moon.svg'
 import ShoppingCart from './assets/shopping-cart.svg'
+import CartDetails from './cine/CartDetails';
 
 function Header() {
+    const [showCart, setShowCart] = useState(false);
+    function handleCartShow(){
+        setShowCart(true)
+    }
     return (
         <header>
+            {
+                showCart && <CartDetails onClose={setShowCart}></CartDetails>
+            }
             <nav className="container flex items-center justify-between space-x-10 py-6">
                 <a href="index.html">
                     <img src={Logo} width="139" height="26" alt="" />
@@ -24,7 +32,7 @@ function Header() {
                         </a>
                     </li>
                     <li>
-                        <a className="bg-green-400/20 dark:bg-green-400/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                        <a onClick={handleCartShow} className="bg-green-400/20 dark:bg-green-400/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
                             <img src={ShoppingCart} width="24" height="24" alt="" />
                         </a>
                     </li>
