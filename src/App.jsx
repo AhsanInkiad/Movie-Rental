25e0d39edec13395fca5283941d8d56a4f3e5361
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-31 15:14:16 +0600

    Convert App.jsx useState to useReducer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import './App.css'
 import MovieList from './cine/MovieList'
 import { MovieContext, ThemeContext } from './context'
 import Footer from './footer'
 import Header from './Header'
 import SideBar from './SideBar'
+import { cartReducer, initialState } from './reducers/CartReducer'
 
 function App() {
-  const [cartData, setCartData] = useState([]);
+  // const [cartData, setCartData] = useState([]);
+  const [state, dispatch] = useReducer(cartReducer, initialState);
   const [darkMode, setDarkMode] = useState(true);
 
   return (
     <>
       <>
-        <MovieContext.Provider value={{ cartData, setCartData }}>
+        <MovieContext.Provider value={{ state, dispatch }}>
           <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
             <div className={`h-full w-full ${darkMode? 'dark':''}`}>
               <Header></Header>
