dc8335a66c20ab04577232e86ee5fa480607ba7a
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-31 15:39:49 +0600

    Add toaster in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './footer'
 import Header from './Header'
 import SideBar from './SideBar'
 import { cartReducer, initialState } from './reducers/CartReducer'
-
+ import { ToastContainer, toast } from 'react-toastify';
 function App() {
   // const [cartData, setCartData] = useState([]);
   const [state, dispatch] = useReducer(cartReducer, initialState);
@@ -27,6 +27,7 @@ function App() {
               </main>
               <Footer></Footer>
             </div>
+            <ToastContainer></ToastContainer>
           </ThemeContext.Provider>
         </MovieContext.Provider>
       </>
